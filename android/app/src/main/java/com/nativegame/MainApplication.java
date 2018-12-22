b9454c330cb73dcd9144ae08a8d3731458a952c3
ih8sum3r <ih8sum3r@gmail.com>
2018-12-22 02:26:12 -0500

    on track! on track

--- android/app/src/main/java/com/nativegame/MainApplication.java
+++ android/app/src/main/java/com/nativegame/MainApplication.java
@@ -1,7 +1,7 @@
 package com.nativegame;
 
+import com.navigationbar.*;
 import android.app.Application;
-
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -21,9 +21,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
+      return Arrays.<ReactPackage>asList(new MainReactPackage(), new NavigationBarPackage());
     }
 
     @Override
---
+++ android/app/src/main/java/com/navigationbar/NavigationBarPackage.java
@@ -0,0 +1,29 @@
+package com.navigationbar;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.*;
+
+public class NavigationBarPackage implements ReactPackage {
+
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new NavigationBarModule(reactContext));
+        return modules;
+    }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
